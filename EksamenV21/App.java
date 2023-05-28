c8dee16108d15a453e121123e3ab057b3903e582
Preben Andersen <preb1anders1@gmail.com>
2023-05-28 14:21:28 +0200

    ferdig2021

--- EksamenV21/App.java
+++ EksamenV21/App.java
@@ -1,5 +1,9 @@
 // importer bilioteker
 
+// a) 
+
+import java.time.LocalDate;
+
 @Entity
 public class Fag {
 
@@ -13,6 +17,15 @@ public class Fag {
     @ManyToOne
     @JoinColumn(name="ansvarlig_id", referencedColumnName = "id")
     private Person ansvarlig;
+
+    //d
+    @ManyToMany
+    @JoinTable(
+        name="undervisning",
+        joinColumns =  @JoinColumn(name="fag_kode"),
+        inverseJoinColumns = @JoinColumn(name="laerer_id")
+    )
+    private List<Person> laerer;
     
 }
 
@@ -21,13 +34,59 @@ public class Person {
     
     private String fornavn;
     private String etternavn;
-    private String fodselsdato;
+    private LocalDate fodselsdato;
 
     @id
     @GeneratedValue(strategy=GenerationType.IDENTITY);
 
-    @OneToMany(mappedBy="ansvarlig");
+    @ManyToMany(mappedBy="ansvarlig")
     private List<Fag> fagMedAnsvarFor;
+
+    // d)
+    @ManyToMany(mappedBy="laerere")
+    private List<Fag> fagUnderviser;
 
 }
 
+// b)
+
+public class FagDAO {
+
+    public void lagreNyttFag(String kode, String navn, Integer stp, String semester, Integer ansvarlig_id) {
+        
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            Person fagansvarlig = em.find(Person.class, ansvarlig_id);
+            if(fagansvarlig == null) {
+                // returner uten å gjøre noe
+            } else {
+                Fag nyttFag = new Fag(kode, navn, stp, semester, ansvarlig_id);
+                // antar at leggTilFagansvarligFor allere finnes i FagDAO
+                fagansvarlig.leggTilFagansvarligFor(nyttFag);
+
+                Fag ingenKode = em.find(Fag.class, kode);
+                if(ingenKode == null) {
+                    // Opprette et nytt fag med nye data --> ?
+                    em.persist(nyttFag);
+                } else {
+                    // Oppdaterer eksisterende fag med nye data
+                    em.merge(nyttFag);
+                }
+            }
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+    }
+}
+
+// c) Nå kan et fag ha flere lærere, og en lærer undervise i et fag. Utvid databasen
+
+// Lage koblingstabell mellom lærer og fag. F.eks. undervisning
+
+// d) 
